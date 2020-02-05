749efbdf052923b0392243e29f71d7e9d55f1180
Julio Herrera <jurhs2000@gmail.com>
2020-02-05 06:52:52 -0600

    icalculator added

--- src/prefixCalculator/iCalculator.java
+++ src/prefixCalculator/iCalculator.java
@@ -1,5 +1,21 @@
 package prefixCalculator;
 
 public interface iCalculator {
+	
+	//pre: requires 2 numbers
+	//post: addition number b to number a
+	int sum(int a, int b);
+	
+	//pre: requires 2 numbers
+	//post: subtract number b to number a
+	int subtract(int a, int b);
+	
+	//pre: requires 2 numbers
+	//post: multiply number a, b times
+	int multiply(int a, int b);
+	
+	//pre: requires 2 numbers, number b can't be 0
+	//post: split number a in b parts, return the quotient
+	int division(int a, int b);
 
 }
