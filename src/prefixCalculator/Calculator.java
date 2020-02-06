dceb8e326fd96683fb7ac90f0e16bc8f2b1b82d1
Jose Hurtarte <JJHH121106@gmail.com>
2020-02-05 18:34:31 -0600

    se creo el controlador del iCalculator

--- src/prefixCalculator/Calculator.java
+++ src/prefixCalculator/Calculator.java
@@ -1,5 +1,29 @@
 package prefixCalculator;
 
-public class Calculator {
+public class Calculator implements iCalculator{
+
+	@Override
+	public int suma(int a, int b) {
+		
+		return a+b;
+	}
+
+	@Override
+	public int resta(int a, int b) {
+		
+		return a-b;
+	}
+
+	@Override
+	public int multiplicacion(int a, int b) {
+		
+		return a*b;
+	}
+
+	@Override
+	public int division(int a, int b) {
+		
+		return a/b;
+	}
 
 }
--- src/prefixCalculator/iCalculator.java
+++ src/prefixCalculator/iCalculator.java
@@ -4,15 +4,15 @@ public interface iCalculator {
 	
 	//pre: requires 2 numbers
 	//post: addition number b to number a
-	int sum(int a, int b);
+	int suma(int a, int b);
 	
 	//pre: requires 2 numbers
 	//post: subtract number b to number a
-	int subtract(int a, int b);
+	int resta(int a, int b);
 	
 	//pre: requires 2 numbers
 	//post: multiply number a, b times
-	int multiply(int a, int b);
+	int multiplicacion(int a, int b);
 	
 	//pre: requires 2 numbers, number b can't be 0
 	//post: split number a in b parts, return the quotient
